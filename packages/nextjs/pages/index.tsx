088f3240f7e0a41c56510542571ef5dde9ebc1de
leeduckgo <leeduckgo@gmail.com>
2023-12-29 14:15:18 +0800

    update

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -16,29 +16,12 @@ export type search_result = {
   metadata: {};
 };
 
-function timeout(delay: number) {
-  return new Promise(res => setTimeout(res, delay));
-}
 
 const ETHSpace: NextPage = () => {
   //在对后端发起请求后，将response的内容保存在results中
   //如果用户选择使用mixed模式，则使用resultByDataset来记录结果
-  const [res, setRes] = useState<resultByDataset[]>([]);
-  //设置默认是在我们提供的数据集而不是公共数据集中查询
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [datasetList, _setDatasetList] = useState(false);
-  //获取目前提供的数据集选项
-  const [options, setOptions] = useState<string[]>([]);
-  const [itemId, setItemId] = useState<number>();
-  //获取用户选择的数据集
-  const [dataset, setDataset] = useState("mixed");
-  //获取用户搜索的prompt
-  const [searchPrompt, setSearchPrompt] = useState("");
-  //仅在组件挂载时执行一次获取数据集列表
+  const [projects, setProjects] = useState([]);
 
-  useEffect(() => {
-    setOptions(["bodhi-text-contents"]);
-  }, []);
   //从后端获取数据集列表
   // useEffect(() => {
   //   fetchOptions();
@@ -57,241 +40,92 @@ const ETHSpace: NextPage = () => {
   //   // const data=await response.json();
   // };
   //获取search prompt与dataset名字后向后端发request
-  const handleonClick = async () => {
-    // 1. request a search item at deno.
-    // 2. wait for 1 sec to query the result.
-    // 3. return as a list.
+  const init = async () => {
 
-    // await fetch("https://polite-goose-87.deno.dev", {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json; charset=utf-8",
-    //   },
-    //   // mode: "no-cors",
-    // })
-    // .then(response => {
-    //   console.log("respp:" + JSON.stringify(response));
-    // })
-
-    console.log("searchPrompt:" + searchPrompt);
-    const response = await fetch("https://embedding-search.deno.dev/", {
-      method: "POST",
+    const response = await fetch("https://query-noncegeek-space.deno.dev", {
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        text: searchPrompt,
-      }),
       // mode: "no-cors",
     });
     const data = await response.json();
-    console.log("data:", data);
-
-    // 1. setItemId
-    setItemId(data.item_id);
-    console.log("item_id: " + data.item_id);
-
-    // 2. wait 1 sec
-    await timeout(1000);
-
-    // 3. query for data
-    const response2 = await fetch("https://query-bodhi-user-search.deno.dev", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id: data.item_id,
-      }),
-      // mode: "no-cors",
-    });
-    const data2 = await response2.json();
-    console.log("data2: ", data2);
-
-    const res1: resultByDataset = {
-      dataset_id: "bodhi-text-contents",
-      results: data2.resp.data.map((item: { uuid: any; data: any; metadata: any }) => {
-        return {
-          id: item.uuid,
-          data: item.data,
-          metadata: item.metadata,
-        };
-      }),
-    };
-    console.log("res1: ", res1);
-    // console.log(data.result.similarities);
-    setRes([res1]);
-
-    // const response = await fetch("https://faas.movespace.xyz/api/v1/run?name=VectorAPI&func_name=search_data", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json; charset=utf-8",
-    //   },
-    //   body: JSON.stringify({
-    //     params: [dataset, searchPrompt, 5],
-    //   }),
-    // });
-    // const data = await response.json();
-    // const res1: resultByDataset = {
-    //   dataset_id: data.result.dataset_id,
-    //   results: data.result.similarities.map((s: { data: any; metadata: any }) => {
-    //     return {
-    //       data: s.data,
-    //       metadata: s.metadata,
-    //     };
-    //   }),
-    // };
-    // // console.log(data.result.similarities);
-    // setRes(res => [res1, ...res]);
-    // // console.log(res);
+    console.log("data:" + JSON.stringify(data));
+    setProjects(data);
   };
+
+  useEffect(() => {
+    init();
+  });
   return (
-    <div className="grid lg:grid-cols-2 flex-grow">
+    <div className="grid lg:grid-cols-1 flex-grow">
       <div className="hero min-h-screen bg-base-200 bg-gradient-to-r from-green-500 to-blue-500">
         <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-2xl font-bold">🔎🤠 Bodhi AI Explorer</h1>
-            <p className="py-6">-- Content Search & Tagger App based on AI </p>
+          <div className="max-w-screen-xl">
+            <h1 className="text-2xl font-bold">🏝️ NonceGeekDAO Project Islands 🏝️</h1>
+            <p className="py-6"> -- See the AwEsOMe Projects powered by NonceGeekDAO!
+              <br></br><br></br> -- 😊power <b>50+ projects in web2/web3😊</b>{" "}
+            </p>
             <div className="join mb-6">
-              <div>
-                <div>
-                  <input
-                    style={{ width: "300px" }}
-                    className="input input-bordered join-item"
-                    value={searchPrompt}
-                    onChange={e => {
-                      setSearchPrompt(e.target.value);
-                    }}
-                    placeholder="Enter your prompt to search"
-                  />
-                </div>
-              </div>
-              <div>
-                <div>
-                  {!datasetList ? (
-                    <select
-                      className="select select-bordered join-item"
-                      onChange={e => {
-                        setDataset(e.target.value);
-                      }}
-                    >
-                      {options.map((option, index) => (
-                        <option key={index} value={option}>
-                          {option}
-                        </option>
-                      ))}
-                    </select>
-                  ) : (
-                    <input
-                      className="input input-bordered join-item"
-                      value={dataset}
-                      onChange={e => {
-                        setDataset(e.target.value);
-                      }}
-                      placeholder="Pls input the public dataset name"
-                    />
-                  )}
-                </div>
-              </div>
-              <div className="indicator">
-                <button
-                  className="btn join-item"
-                  onClick={() => {
-                    handleonClick();
-                  }}
-                >
-                  🔎 Search
-                </button>
-              </div>
-            </div>
-            <div className="hero-content text-left">
-              <span className="text-sm">
-                <p>
-                  <b>Some search question examples: </b>
-                </p>
-                <p>* bitcoin</p>
-                <p>* bodhi</p>
-                <p>* leeduckgo</p>
-              </span>
-            </div>
-            <a href="https://bodhi.wtf/10586" target="_blank" rel="noreferrer">
-              <button className="w-96 bg-white hover:bg-gray-100 text-gray-800 py-2 px-5 border border-gray-400 rounded shadow">
-                🤑 <b>Buy</b> shares to support explorer!
-              </button>
-            </a>
-            <br></br>
-            <a href="https://bodhi.wtf/13663" target="_blank" rel="noreferrer">
-              <button className="w-96 bg-white hover:bg-gray-100 text-gray-800 py-2 px-5 border border-gray-400 rounded shadow">
-                📝 Blog for Explorer<b>(See the future plan)</b>
-              </button>
-            </a>
-            <br></br>
-            <a href="https://explorer.movespace.xyz" target="_blank" rel="noreferrer">
-              <button className="w-96 bg-white hover:bg-gray-100 text-gray-800 py-2 px-5 border border-gray-400 rounded shadow">
-                <b>🛝 Go to MoveSpace Explorer</b>
-              </button>
-            </a>
-            <br></br>
-            <a href="https://random-hacker.deno.dev/" target="_blank" rel="noreferrer">
-            <button className="w-96 bg-white hover:bg-gray-100 text-gray-800 py-2 px-5 border border-gray-400 rounded shadow">
-              👽 A Random <b>Indie Hacker</b>
-            </button>
-            </a>
-            <br></br>
-            <a href="https://twitter.com/0xleeduckgo" target="_blank" rel="noreferrer">
-              <button className="w-96 bg-white hover:bg-gray-100 text-gray-800 py-2 px-5 border border-gray-400 rounded shadow">
-                ❤️ Follow my twitter! ❤️
-              </button>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="bg-gradient-to-r from-blue-500 to-green-500">
-        <div className="mx-auto w-4/5 max-h-[600px] backdrop-blur-lg backdrop-filter p-10 m-10 rounded-lg opacity-80 shadow-md overflow-auto overflow-y-auto">
-          <h2 className="text-4xl font-bold mb-1">Search Results</h2>
-          <div>
-            {res.map((r, index) => (
-              <div key={index} className="collapse collapse-open bg-base-200 m-5 overflow-x-auto">
-                <input type="checkbox" className="peer" />
-                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                  Results from {r.dataset_id}
-                </div>
-                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                  {r.results.map((item, index) => (
-                    <div key={index}>
-                      <div className="divider"></div>
-                      <span className="text-xl">Data</span>
-                      <div>
-                        <ReactMarkdown>{item.data}</ReactMarkdown>
-                      </div>
-                      <span className="text-xl">Metadata</span>
-                      <pre className="text-base">
-                        <b>Creator:</b> {item.metadata.creator}
-                      </pre>
-                      <pre className="text-base">
-                        <b>Bodhi ID(view the full content in Bodhi👉): </b>
-                        <a href={"https://bodhi.wtf/" + item.metadata.id} target="_blank" rel="noreferrer">
-                          <button className="btn join-item">{item.metadata.id}</button>
-                        </a>
-                      </pre>
-                      <pre className="text-base">
-                        <b>Type: </b>
-                        {item.metadata.type}
-                      </pre>
-                      <br></br>
-                      <span className="text-xl">id in vectorDB</span>
-                      <pre className="text-base">
-                        <b>{item.id}</b>
-                      </pre>
-                      <br></br>
-                      <a href={"/debug?uuid=" + item.id} target="_blank" rel="noreferrer">
-                        <button className="btn join-item">Tag this item!(Comming Soon..)</button>
+              <div className="grid gap-5 mt-5 md:grid-cols-2 lg:grid-cols-2">
+                {projects.map(({ id, name, description, status, url, github, tags, author }) => (
+                  <div
+                    key={id}
+                    className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <div className="p-5">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+                      <a
+                        href={url}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="inline-flex justify-center items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-32"
+                      >
+                        Launch App🚀
                       </a>
+                      &nbsp;&nbsp;&nbsp;
+                      <a
+                        href={github}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="inline-flex justify-center items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-32"
+                      >
+                        Github
+                      </a>
+                      <br></br>
+                      <br></br>
+                      <a
+                        href={author.twitter}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="inline-flex justify-center items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-64"
+                      >
+                        👀View the Author
+                      </a>
+                      <br></br>
+                      <br></br>
+                      <div>
+                        <b>Tags: </b>
+                        {tags.map((tag, index) => (
+                          <span key={index} className="inline-flex items-center px-3 py-1 text-xs font-semibold leading-none text-white bg-orange-600 rounded-full">
+                            {tag}
+                          </span>
+                        ))}
+                        
+                      </div>
+                      <br></br>
+                      <div>
+                        <b>Status: </b>
+                        <span className="inline-flex items-center px-3 py-1 text-xs font-semibold leading-none text-white bg-orange-600 rounded-full">
+                          {status}
+                        </span>
+                      </div>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
         </div>
       </div>
